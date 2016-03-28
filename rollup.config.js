a5bb15497f3de6317cea314e4dbe53d5614ae68a
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-03-28 15:24:07 +0300

    Configure rollup to use es2015-rollup plugin and ignore .babelrc files

--- rollup.config.js
+++ rollup.config.js
@@ -3,5 +3,8 @@ import babel from 'rollup-plugin-babel';
 export default {
   entry: 'src/index.js',
   sourceMap: true,
-  plugins: [babel()]
+  plugins: [babel({
+    presets: ['es2015-rollup'],
+    babelrc: false
+  })]
 };
--- rollup.config.test.js
+++ rollup.config.test.js
@@ -2,7 +2,10 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'test/index.js',
-  plugins: [babel()],
+  plugins: [babel({
+    presets: ['es2015-rollup'],
+    babelrc: false
+  })],
   format: 'cjs',
   intro: 'require("source-map-support").install();',
   dest: 'build/test-bundle.js',
