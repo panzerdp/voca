79cbd6f0a7f69c7a4cb2348fed87b2f7452cef5f
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-03-29 16:20:46 +0300

    isDigit comments cleanup

--- src/query/is_digit.js
+++ src/query/is_digit.js
@@ -1,14 +1,7 @@
 import toString from '../utils/to_string'
 import undefinedDefault from '../utils/undefined_default';
 
-/**
- * RegEx to match alpha chars in unicode.
- *
- * @see http://stackoverflow.com/a/22075070/1894471
- * @type {RegExp}
- */
 var REGEX_DIGIT = /^[0-9]+$/;
-
 
 /**
  * Checks if `value` contains only digit characters.
