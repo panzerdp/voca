f8c071635820a9aaec404b11d235b9888e57baf7
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-03-31 11:10:03 +0300

    Implemented isEndingWith function

--- src/query/is_ending_with.js
+++ src/query/is_ending_with.js
@@ -5,10 +5,11 @@ import undefinedDefault from '../utils/undefined_default';
 /**
  * Checks if `string` ends with `end`.
  * @param {string} [string=''] The string to verify.
- * @param {string} end The ending string.
+ * @param {string} [end] The ending string.
+ * @param {int} [position=string.length] Search within `string` as if this string were only `position` long.
  * @return {boolean} Returns `true` if `string` ends with `end`, `false` otherwise.
  */
-export default function(string, end) {
+export default function(string, end, position) {
   if (end == null) {
     return false;
   }
@@ -17,5 +18,13 @@ export default function(string, end) {
   if (stringValue === null || stringEnd === null) {
     return false;
   }
-  return stringValue.lastIndexOf(stringEnd, stringValue.length - stringEnd.length - 1) !== -1;
+  if (typeof position !== 'number'
+    || !isFinite(position)
+    || Math.floor(position) !== position
+    || position > stringValue.length) {
+    position = stringValue.length;
+  }
+  position -= stringEnd.length;
+  var lastIndex = stringValue.indexOf(stringEnd, position);
+  return lastIndex !== -1 && lastIndex === position;
 }
