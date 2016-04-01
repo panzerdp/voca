1e8c01adeb4859f7099f2963bc2b5faf436347dc
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-04-01 12:40:05 +0300

    Punctuation fix

--- src/query/is_blank.js
+++ src/query/is_blank.js
@@ -2,9 +2,9 @@ import toString from '../utils/to_string';
 import undefinedDefault from '../utils/undefined_default';
 
 /**
- * Checks if `string` is empty or contains only whitespaces
+ * Checks if `string` is empty or contains only whitespaces.
  * @param {string} [string=''] The string to verify.
- * @return {boolean} Returns `true` if `string` is empty or contains only whitespaces, `false` otherwise
+ * @return {boolean} Returns `true` if `string` is empty or contains only whitespaces, `false` otherwise.
  */
 export default function(string) {
   string = undefinedDefault(string, '');
