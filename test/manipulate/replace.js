539550d8cc59292e309dddfed94e0b8620e64456
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-04-23 19:02:50 +0300

    Implemented replace() function

--- test/manipulate/replace.js
+++ test/manipulate/replace.js
@@ -6,6 +6,7 @@ describe('replace', function() {
 
   it('should return the result with a string pattern', function() {
     expect(v.replace('duck', 'duck', 'swan')).to.be.equal('swan');
+    expect(v.replace(PRINTABLE_ASCII, PRINTABLE_ASCII, PRINTABLE_ASCII)).to.be.equal(PRINTABLE_ASCII);
   });
 
 });
