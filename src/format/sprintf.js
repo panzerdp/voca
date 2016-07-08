0a09744a1690c407eef5ea661faf5f836898c750
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-07-08 16:50:10 +0300

    Detailed documentation for sprintf()

--- src/format/sprintf.js
+++ src/format/sprintf.js
@@ -56,21 +56,115 @@ function replaceConversionSpecification(index, args, signSpecifier, paddingSpeci
 /**
  * Produces a string according to `format`.
  *
- * The format string contains characters, beginning and ending in its initial shift state, if any. The format
+ * <div class="smaller">
+ * `format` string contains characters, beginning and ending in its initial shift state, if any. The format
  * string is composed of zero or more directives: ordinary characters (not <code>%</code>), which are  copied  unchanged
- * to  the  output stream; and conversion specifications, each of which results in fetching zero or more subsequent
+ * to  the  output string and conversion specifications, each of which results in fetching zero or more subsequent
  * arguments. Each conversion specification is introduced by the character <code>%</code>, and ends with a <b>conversion
- * specifier</b>. In between there may be (in this order) <b>zero or more flags</b>, an optional <b>minimum field width</b>, an
- * optional <b>precision</b> and an optional <b>length modifier</b>.
+ * specifier</b>. In between there may be (in this order) zero or more <b>flags</b>, an optional <b>minimum field width</b>
+ * and an optional <b>precision</b>.<br/>
+ * By default, the arguments are used in the order given.<br/>
+ * For argument numbering and swapping, write `%m$` (where `m` is a number indicating the argument order)
+ * instead of `%` to specify explicitly which argument is taken.<br/><br/>
  *
  * <b>The flag characters</b><br/>
  * The character <code>%</code> is followed by zero or more of the following flags:<br/>
- * <code>0</code> The value should be zero padded.<br/>
- * <code>-</code> The converted value is to be left adjusted on the field boundary (the default is right justification).<br/>
- * <code>' '</code> (a space) The value should be space padded.<br/>
- * <code>+</code> A  sign (<code>+</code> or <code>-</code>) should always be placed before a number produced by a
- * signed conversion. By default a sign is used only for negative numbers.<br/>
- * <code>'</code> Indicates that the immediately following character is used for padding.<br/>
+ * <table class="light-params">
+ *   <tr>
+ *     <td><code>+</code></td>
+ *     <td>
+ *       A  sign (<code>+</code> or <code>-</code>) should always be placed before a number produced by a
+ *       signed conversion. By default a sign is used only for negative numbers.
+ *     </td>
+ *   </tr>
+ *   <tr>
+ *     <td><code>0</code></td>
+ *     <td>The value should be zero padded.</td>
+ *   </tr>
+ *   <tr>
+ *     <td><code>&blank;</code></td>
+ *     <td>(a space) The value should be space padded.</td>
+ *   </tr>
+ *   <tr>
+ *    <td><code>'</code></td>
+ *    <td>Indicates alternate padding character, specified by prefixing it with a single quote <code>'</code>.</td>
+ *   </tr>
+ *   <tr>
+ *     <td><code>-</code></td>
+ *     <td>The converted value is to be left adjusted on the field boundary (the default is right justification).</td>
+ *   </tr>
+ * </table>
+ *
+ * <b>The field width</b><br/>
+ * An  optional decimal digit string (with nonzero first digit) specifying a minimum field width.  If the converted
+ * value has fewer characters than the field width, it will be padded with spaces on the left (or right, if the
+ * left-adjustment flag has been given).<br/><br/>
+ *
+ * <b>The precision</b><br/>
+ * An optional precision, in the form of a period `.` followed by an optional decimal digit string.<br/>
+ * This gives the number of digits to appear after the radix character for `e`, `E`, `f` and `F` conversions, the
+ * maximum number of significant digits for `g` and `G` conversions or the maximum number of characters to be printed
+ * from a string for `s` conversion.<br/><br/>
+ *
+ * <b>The conversion specifier</b><br/>
+ * A specifier that mentions what type the argument should be treated as:
+ *
+ * <table class="light-params">
+ *   <tr>
+ *     <td>`s`</td>
+ *     <td>The string argument is treated as and presented as a string.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>`d` `i`</td>
+ *     <td>The integer argument is converted to signed decimal notation.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>`b`</td>
+ *     <td>The unsigned integer argument is converted to unsigned binary.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>`c`</td>
+ *     <td>The unsigned integer argument is converted to an ASCII character with that number.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>`o`</td>
+ *     <td>The unsigned integer argument is converted to unsigned octal.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>`u`</td>
+ *     <td>The unsigned integer argument is converted to unsigned decimal.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>`x` `X`</td>
+ *     <td>The unsigned integer argument is converted to unsigned hexadecimal. The letters `abcdef` are used for `x`
+ *     conversions; the letters `ABCDEF` are used for `X` conversions.</td>
+ *   </tr>
+ *   <tr>
+ *     <td>`e` `E`</td>
+ *     <td>
+ *       The float argument is rounded and converted in the style `[-]d.ddde±dd`, where there is one digit
+ *       before the decimal-point character and the number of digits after it is equal to the precision. If
+ *       the precision is missing, it is taken as 6; if the precision is zero, no decimal-point character
+ *       appears. An `E` conversion uses the letter `E` (rather than `e`) to introduce the exponent.
+ *     </td>
+ *   </tr>
+ *   <tr>
+ *     <td>`g` `G`</td>
+ *     <td>
+ *       The double argument is converted in style `f` or `e` (or `F` or `E` for `G` conversions). The precision specifies
+ *       the number of significant digits. If the precision is missing, 6 digits are given; if the
+ *       precision is zero, it is treated as 1. Style `e` is used if the exponent from its conversion is less
+ *       than -6 or greater than or equal to the precision. Trailing zeros are removed from the fractional
+ *       part of the result; a decimal point appears only if it is followed by at least one digit.
+ *     </td>
+ *   </tr>
+ *   <tr>
+ *     <td>`%`</td>
+ *     <td>A literal `%` is written. No argument is converted. The complete conversion specification is `%%`.</td>
+ *   </tr>
+ *
+ * </table>
+ * </div>
  *
  * @function sprintf
  * @static
