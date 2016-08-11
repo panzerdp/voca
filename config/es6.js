0b4bf08f64f2fbb36dd508cec7024094dd472b68
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-08-11 12:04:43 +0300

    Removed rollup and the build directories

--- config/es6.js
+++
@@ -1,6 +0,0 @@
-import config from './config';
-
-config.format = 'es';
-config.dest = 'dist/voca.es6.js';
-
-export default config;
--- config/umd.js
+++
@@ -1,7 +0,0 @@
-import config from './config';
-
-config.format = 'umd';
-config.dest = 'dist/voca.js';
-config.moduleName = 'v';
-
-export default config;
