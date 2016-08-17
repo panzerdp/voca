fe0c69f454d349bd08e998349be075d768a047f7
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-08-17 16:55:10 +0300

    The list of browsers for sauce labs unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,44 @@
 module.exports = function (grunt) {
   var browsers = [{
-    browserName: 'googlechrome',
-    platform: 'linux'
+    // Chrome
+    browserName: 'chrome',
+    platform: 'Windows 10',
+    version: '52'
+  }, {
+    browserName: 'chrome',
+    platform: 'Linux',
+    version: '48'
+  }, {
+    // Firefox
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '47'
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '48'
+  }, {
+    // IE
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '9'
   }, {
     browserName: 'internet explorer',
-    platform: 'WIN8',
+    platform: 'Windows 8',
     version: '10'
+  }, {
+    browserName: 'internet explorer',
+    platform: 'Windows 10',
+    version: '11'
+  }, {
+    // Safari
+    browserName: 'safari',
+    platform: 'OS X 10.10',
+    version: '8'
+  }, {
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '9'
   }];
 
   grunt.initConfig({
@@ -26,7 +59,7 @@ module.exports = function (grunt) {
             'http://127.0.0.1:9999/test_runner/index.html'
           ],
           browsers: browsers,
-          build: process.env.TRAVIS_JOB_ID,
+          build: Math.floor(Math.random() * 10000), //process.env.TRAVIS_JOB_ID,
           testname: 'mocha tests',
           throttled: 3,
           sauceConfig: {
