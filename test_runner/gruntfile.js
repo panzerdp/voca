2de7186f490550986e49f2cd9dc3e8d7d1e4b1cb
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-08-18 17:25:29 +0300

    iPhone Safari browser test

--- test_runner/gruntfile.js
+++ test_runner/gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function (grunt) {
   var browsers = [{
-    // Chrome
     browserName: 'chrome',
     platform: 'Windows 10',
     version: '52'
@@ -9,7 +8,6 @@ module.exports = function (grunt) {
     platform: 'Linux',
     version: '48'
   }, {
-    // Firefox
     browserName: 'firefox',
     platform: 'Windows 10',
     version: '47'
@@ -18,7 +16,6 @@ module.exports = function (grunt) {
     platform: 'Windows 10',
     version: '48'
   }, {
-    // IE
     browserName: 'internet explorer',
     platform: 'Windows 7',
     version: '9'
@@ -31,12 +28,10 @@ module.exports = function (grunt) {
     platform: 'Windows 10',
     version: '11'
   }, {
-    // Edge
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
     version: '13.10586'
   }, {
-    // Safari
     browserName: 'safari',
     platform: 'OS X 10.9',
     version: '7'
@@ -52,6 +47,10 @@ module.exports = function (grunt) {
     browserName: 'Android',
     platform: 'Linux',
     version: '5.1'
+  }, {
+    browserName: "iphone",
+    platform: "OS X 10.11",
+    version: "9.3"
   }];
 
   var buildId = process.env.TRAVIS_JOB_ID ? process.env.TRAVIS_JOB_ID : Math.floor(Math.random() * 10000);
