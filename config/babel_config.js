2b3fb069d8eb93dd0610c9a67028b8c5b17b4af8
Dmitri Pavlutin <dpavlutin@gmail.com>
2016-12-22 14:36:16 +0200

    Refactored wordWrap() function

--- config/babel_config.js
+++ config/babel_config.js
@@ -7,6 +7,7 @@ export default babel({
     "transform-es2015-parameters",
     "transform-es2015-shorthand-properties",
     "transform-es2015-spread",
+    "transform-es2015-destructuring",
     ["module-resolver", {
       "root": ["./src"]
     }]
--- src/manipulate/word_wrap.js
+++ src/manipulate/word_wrap.js
@@ -1,12 +1,27 @@
 import coerceToBoolean from 'helper/boolean/coerce_to_boolean';
 import coerceToNumber from 'helper/number/coerce_to_number';
 import coerceToString from 'helper/string/coerce_to_string';
-import nilDefault from 'helper/undefined/nil_default';
 
 const OPTION_WIDTH = 'width';
 const OPTION_NEW_LINE = 'newLine';
 const OPTION_INDENT = 'indent';
 const OPTION_CUT = 'cut';
+
+/**
+ * Determine the word wrap options. The missing values are filled with defaults.
+ *
+ * @param  {Object} options  The options object.
+ * @return {Object}          The word wrap options, with default settings if necessary.
+ * @ignore
+ */
+function determineOptions(options) {
+  return {
+    width: coerceToNumber(options[OPTION_WIDTH], 75),
+    newLine: coerceToString(options[OPTION_NEW_LINE], '\n'),
+    indent: coerceToString(options[OPTION_INDENT], ''),
+    cut: coerceToBoolean(options[OPTION_CUT], false)
+  };
+}
 
 /**
  * Wraps `subject` to a given number of characters using a string break character.
@@ -44,22 +59,16 @@ const OPTION_CUT = 'cut';
  * // => 'Wonde\nrful\nworld'
  *
  */
-export default function wordWrap(subject, options) {
+export default function wordWrap(subject, options = {}) {
   const subjectString = coerceToString(subject);
-  options = nilDefault(options, {});
-  const width = coerceToNumber(options[OPTION_WIDTH], 75);
-  const newLine = coerceToString(options[OPTION_NEW_LINE], '\n');
-  const indent = coerceToString(options[OPTION_INDENT], '');
-  const cut = coerceToBoolean(options[OPTION_CUT], false);
-
+  const { width, newLine, indent, cut } = determineOptions(options);
   if (subjectString === '' || width <= 0) {
     return indent;
   }
-
   const subjectLength = subjectString.length;
+  const substring = subjectString.substring.bind(subjectString);
   let offset = 0;
   let wrappedLine = '';
-
   while ((subjectLength - offset) > width) {
     if (subjectString[offset] === ' ') {
       offset++;
@@ -67,26 +76,26 @@ export default function wordWrap(subject, options) {
     }
     let spaceToWrapAt = subjectString.lastIndexOf(' ', width + offset);
     if (spaceToWrapAt >= offset) {
-      wrappedLine += indent + subjectString.substring(offset, spaceToWrapAt) + newLine;
+      wrappedLine += indent + substring(offset, spaceToWrapAt) + newLine;
       offset = spaceToWrapAt + 1;
     } else {
       if (cut) {
-        wrappedLine += indent + subjectString.substring(offset, width + offset) + newLine;
+        wrappedLine += indent + substring(offset, width + offset) + newLine;
         offset += width;
       } else {
         spaceToWrapAt = subjectString.indexOf(' ', width + offset);
         if (spaceToWrapAt >= 0) {
-          wrappedLine += indent + subjectString.substring(offset, spaceToWrapAt) + newLine;
+          wrappedLine += indent + substring(offset, spaceToWrapAt) + newLine;
           offset = spaceToWrapAt + 1;
         } else {
-          wrappedLine += indent + subjectString.substring(offset);
+          wrappedLine += indent + substring(offset);
           offset = subjectLength;
         }
       }
     }
   }
   if (offset < subjectLength) {
-    wrappedLine += indent + subjectString.substring(offset);
+    wrappedLine += indent + substring(offset);
   }
   return wrappedLine;
 }
