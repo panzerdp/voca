d5b09c14fccc0ca00d887da855cb8c1715ec6815
Dmitri Pavlutin <dpavlutin@gmail.com>
2019-12-25 16:18:19 +0200

    Build modules

---
+++ config/rollup_dist_individual.js
@@ -0,0 +1,27 @@
+import babelConfig from './babel_config';
+import glob from 'glob';
+
+const DIRECTORY_SRC = 'src/';
+const DIRECTORY_DIST = 'dist_mod/';
+
+function modulesPaths() {
+  const paths = glob.sync(DIRECTORY_SRC + '*/*.js', {
+    ignore: [
+      DIRECTORY_SRC + 'chain/*.js',
+      DIRECTORY_SRC + 'helper/**/*.js',
+      DIRECTORY_SRC + 'util/**/*.js',
+      DIRECTORY_SRC + 'functions.js',
+      DIRECTORY_SRC + 'index.js'
+    ]
+  });
+  return paths;
+}
+
+export default {
+  input: modulesPaths(),
+  plugins: [babelConfig],
+  output: {
+    dir: DIRECTORY_DIST,
+    format: 'cjs'
+  }
+};
