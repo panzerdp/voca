75a7bc7187977503cca58f635df07cf7e4b35185
Dmitri Pavlutin <dpavlutin@gmail.com>
2019-12-26 11:31:53 +0200

    Remove codecov.io and other obsolete modules

--- test/escape/escape_html.js
+++ test/escape/escape_html.js
@@ -1,23 +1,26 @@
-
 import { PRINTABLE_ASCII } from '../const';
 import v from '../voca';
 
 describe('escapeHtml', function() {
-
   it('should return the escaped string', function() {
     expect(v.escapeHtml('<>&"\'`')).toBe('&lt;&gt;&amp;&quot;&#x27;&#x60;');
-    expect(v.escapeHtml('<p>wonderful world</p>')).toBe('&lt;p&gt;wonderful world&lt;/p&gt;');
+    expect(v.escapeHtml('<p>wonderful world</p>')).toBe(
+      '&lt;p&gt;wonderful world&lt;/p&gt;'
+    );
     expect(v.escapeHtml(PRINTABLE_ASCII)).toBe(
-      ' !&quot;#$%&amp;&#x27;()*+,-./0123456789:;&lt;=&gt;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_&#x60;abcdefghijklmnopqrstuvwxyz{|}~');
+      ' !&quot;#$%&amp;&#x27;()*+,-./0123456789:;&lt;=&gt;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_&#x60;abcdefghijklmnopqrstuvwxyz{|}~'
+    );
   });
 
   it('should return the escaped string representation of an object', function() {
     expect(v.escapeHtml(['<span>'])).toBe('&lt;span&gt;');
-    expect(v.escapeHtml({
-      toString: function() {
-        return '<script>';
-      }
-    })).toBe('&lt;script&gt;');
+    expect(
+      v.escapeHtml({
+        toString: function() {
+          return '<script>';
+        }
+      })
+    ).toBe('&lt;script&gt;');
   });
 
   it('should return an empty string for null or undefined', function() {
@@ -25,5 +28,4 @@ describe('escapeHtml', function() {
     expect(v.escapeHtml(undefined)).toBe('');
     expect(v.escapeHtml(null)).toBe('');
   });
-
 });
